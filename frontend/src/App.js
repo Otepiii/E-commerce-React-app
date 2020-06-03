e950615cdd822cfbdacc7640bfa5fe31c06290d0
joseph Evangelista <oootep123@gmail.com>
2020-06-02 23:25:18 -0400

    added redux with hooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,8 @@ import data from "./data";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
-import HomeScreen from "./Screens/HomeScreen";
-import ProductScreen from "./Screens/ProductScreen";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   const openMenu = () => {
--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,22 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
+// import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { listProducts } from "../actions/productActions";
 
 function HomeScreen(props) {
-  const [products, setProduct] = useState([]);
-
+  const productList = useSelector((state) => state.productList);
+  const { products, loading, error } = productList;
+  const dispatch = useDispatch();
   useEffect(() => {
-    const fetchData = async () => {
-      const { data } = await axios.get("/api/products");
-      setProduct(data);
-    };
-    fetchData();
-    return () => {
-      //
-    };
+    dispatch(listProducts());
+    return () => {};
   }, []);
 
-  return (
+  return(
+  loading ? (
+    <div>Loading...</div>
+  ) : error ? (
+    <div>{error}</div>
+  ) : (
     <ul className="products">
       {products.map((product) => (
         <li>
@@ -40,7 +42,7 @@ function HomeScreen(props) {
         </li>
       ))}
     </ul>
-  );
+  ))
 }
 
 export default HomeScreen;
